8c5113b09b6e33836893af64439924932dbd9358
Suad <suadcodes@gmail.com>
2022-06-15 21:11:57 +0100

    15th June

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import Layout from "./components/layouts/Layout.js"
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Layout from "./components/layouts/Layout.js";
 import PageNotFound from "./components/pages/404.js";
-import Home from "./components/pages/Home.js"
+import Home from "./components/pages/Home.js";
 import SignIn from "./components/pages/Signin.js";
 import ContactUs from "./components/pages/ContactUs.js";
 
 import './App.css';
 function App() {
   return (
+    <BrowserRouter>
  <Layout>
- <Home/>
+  <Routes>
+    <Route path ='/'element ={<Home/>}/>
+    <Route path ='/'element ={<SignIn/>}/>
+    <Route path ='/'element ={ <ContactUs/>}/>
+    <Route path ='*'element ={ <PageNotFound/>}/>
+    </Routes>
  </Layout>
-
+ </BrowserRouter>
  
   );
 }
